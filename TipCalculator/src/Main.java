15953b3c3259933bd3da8bc6e286390ff9ae03eb
Max Croston <maxxlax@gmail.com>
2022-03-10 14:44:48 -0500

    a

--- TipCalculator/src/Main.java
+++ TipCalculator/src/Main.java
@@ -11,88 +11,120 @@ import io.BranchWriter;
 import io.PayrollWriter;
 import io.TotalTipReader;
 
-public class Main {
-	public static ArrayList<Store> group = new ArrayList<Store>();
-	private static String baseFilePath = "C:\\Users\\crost\\OneDrive\\Documents\\Jimmy Johns\\TipResources";
-	private static String attendanceFilePath = baseFilePath + "\\TipAttendance";
-	private static String payoutFilePath = baseFilePath + "\\TipPayout";
+public class Main
+{
+  public static ArrayList<Store> group = new ArrayList<Store>();
+  private static String baseFilePath = "C:\\Users\\crost\\OneDrive\\Documents\\Jimmy Johns\\TipResources";
+  private static String attendanceFilePath = baseFilePath + "\\TipAttendance";
+  private static String payoutFilePath = baseFilePath + "\\TipPayout";
 
-	public static void main(String[] args) {
-		// Gather Tip Data
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Payout Period (mm-dd-yyyy_mm-dd-yyyy");
-		//String payPeriod = keyboard.nextLine();
-		//String payPeriod = "02-14-2022_02-15-2022";
-		String payPeriod = "02-16-2022_03-01-2022";
-		
-		// Read Attendance Files
-		File TipAttendanceFolder = new File(attendanceFilePath + payPeriod);
-		
-		// Read Attendance Reports
-		for (File f : TipAttendanceFolder.listFiles()) {
-			AttendanceReader attendanceData = new AttendanceReader(f);
-			group.add(attendanceData.getStore());
-		}
-		//Read Total Tips from txt or have them enter via keyboard
-		try {
-			TotalTipReader ttr = new TotalTipReader(baseFilePath + "\\TotalTips" + payPeriod + ".txt");
-			ttr.applyTotalTipsToStores(group);
-		} catch (Exception e) {
-			e.printStackTrace();
-			for (Store s : group) {
-				System.out.println("Total Tips For " + s.getStoreNum());
-				s.setTotalTips(keyboard.nextDouble());
-			}
-		}
+  public static void main(String[] args)
+  {
+    // Gather Tip Data
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Payout Period (mm-dd-yyyy_mm-dd-yyyy");
+    // String payPeriod = keyboard.nextLine();
+    // String payPeriod = "02-14-2022_02-15-2022";
+    String payPeriod = "02-16-2022_03-01-2022";
 
-		// Read Branch Data
-		BranchReader branchReader = new BranchReader(new File(baseFilePath + "\\Branch.csv"));
-		HashMap<String, Integer> branchData = branchReader.getBranchInfo();
-		for (String s : branchData.keySet()) {
-			System.out.println(s);
-		}
-		System.out.println("_______________");
+    // Read Attendance Files
+    File TipAttendanceFolder = new File(attendanceFilePath + payPeriod);
 
-		printStores();
-		writeStores(payPeriod);
+    // Read Attendance Reports
+    for (File f : TipAttendanceFolder.listFiles())
+    {
+      AttendanceReader attendanceData = new AttendanceReader(f);
+      group.add(attendanceData.getStore());
+    }
+    // Read Total Tips from txt or have them enter via keyboard
+    try
+    {
+      TotalTipReader ttr = new TotalTipReader(baseFilePath + "\\TotalTips" + payPeriod + ".txt");
+      ttr.applyTotalTipsToStores(group);
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+      for (Store s : group)
+      {
+        System.out.println("Total Tips For " + s.getStoreNum());
+        s.setTotalTips(keyboard.nextDouble());
+      }
+    }
 
-		BranchWriter branchWriter = new BranchWriter(payPeriod, payoutFilePath, group, branchData);
-		branchWriter.writeBulkDisbursementCSV();
+    // Read Branch Data
+    BranchReader branchReader = new BranchReader(new File(baseFilePath + "\\Branch.csv"));
+    HashMap<String, Integer> branchData = branchReader.getBranchInfo();
+    for (String s : branchData.keySet())
+    {
+      System.out.println(s);
+    }
+    System.out.println("_______________");
 
-		PayrollWriter payrollWriter = new PayrollWriter(payPeriod, payoutFilePath, group);
-		payrollWriter.writePayrollExportCSV();
+    printStores();
+    writeStores(payPeriod);
 
-		keyboard.close();
-	}
+    BranchWriter branchWriter = new BranchWriter(payPeriod, payoutFilePath, group, branchData);
+    branchWriter.writeBulkDisbursementCSV();
 
-	private static void printStores() {
-		for (Store s : group) {
-			System.out.println(s.getStoreNum() + " $" + s.getTotalTips() + " Increase per hour: $" + (s.getTotalTips()/s.getTotalInshopHours()));
-			for (Employee e : s.getEmployees()) {
-				System.out
-						.println(String.format("%25s total Inshop Hours: %5.2f Tip Portion: %1.6f Actual Amount: %4.2f",
-								e.getFullName(), e.getTotalInshopHours(), s.getTipPortion(e.getFullName()),
-								s.getDollarTipPortion(e.getFullName())));
-			}
-			System.out.println("Total Hours: " + s.getTotalInshopHours());
-			System.out.println();
-		}
-		System.out.println("Average hourly increase for all stores: $" + getAverageHourlyIncrease());
-	}
+    PayrollWriter payrollWriter = new PayrollWriter(payPeriod, payoutFilePath, group);
+    payrollWriter.writePayrollExportCSV();
 
-	private static Double getAverageHourlyIncrease() {
-		double sumTotalTips = 0;
-		double sumInshopHours = 0;
-		for (Store s : group) {
-			sumTotalTips += s.getTotalTips();
-			sumInshopHours += s.getTotalInshopHours();
-		}
-		return sumTotalTips / sumInshopHours;
-	}
+    keyboard.close();
+  }
 
-	private static void writeStores(String weekending) {
-		for (Store s : group) {
-			s.writeToNote(payoutFilePath, weekending);
-		}
-	}
+  private static void printStores()
+  {
+    String warnings = "";
+    double sum = 0;
+    for (Store s : group)
+    {
+      System.out.println(s.getStoreNum() + " $" + s.getTotalTips() + " Increase per hour: $"
+          + (s.getTotalTips() / s.getTotalInshopHours()));
+      for (Employee e : s.getEmployees())
+      {
+        System.out.println(
+            String.format("%25s total Inshop Hours: %5.2f Tip Portion: %1.6f Actual Amount: %4.2f",
+                e.getFullName(), e.getTotalInshopHours(), s.getTipPortion(e.getFullName()),
+                s.getDollarTipPortion(e.getFullName())));
+
+        if (s.getDollarTipPortion(e.getFullName()) > 100)
+        {
+          warnings += s.getStoreNum() + ": $" + s.getTotalTips() + "|" + s.getTotalInshopHours() + "hrs"
+              + String.format(
+                  "%25s total Inshop Hours: %5.2f Tip Portion: %1.6f Actual Amount: %4.2f",
+                  e.getFullName(), e.getTotalInshopHours(), s.getTipPortion(e.getFullName()),
+                  s.getDollarTipPortion(e.getFullName())) + "\n";
+        }
+      }
+      System.out.println("Total Hours: " + s.getTotalInshopHours());
+      System.out.println();
+      sum += s.getTotalTips();
+    }
+    System.out.println("Average hourly increase for all stores: $" + getAverageHourlyIncrease()
+        + " Total: $" + sum);
+    
+    System.out.println("\nWarnings");
+    System.out.println(warnings);
+  }
+
+  private static Double getAverageHourlyIncrease()
+  {
+    double sumTotalTips = 0;
+    double sumInshopHours = 0;
+    for (Store s : group)
+    {
+      sumTotalTips += s.getTotalTips();
+      sumInshopHours += s.getTotalInshopHours();
+    }
+    return sumTotalTips / sumInshopHours;
+  }
+
+  private static void writeStores(String weekending)
+  {
+    for (Store s : group)
+    {
+      s.writeToNote(payoutFilePath, weekending);
+    }
+  }
 }
--- TipCalculator/src/io/PayrollWriter.java
+++ TipCalculator/src/io/PayrollWriter.java
@@ -7,58 +7,69 @@ import java.util.ArrayList;
 import docobj.Employee;
 import docobj.Store;
 
-public class PayrollWriter {
+public class PayrollWriter
+{
 
-	private String payrollPath;
-	private PrintWriter writer;
-	private ArrayList<Store> group;
+  private String payrollPath;
+  private PrintWriter writer;
+  private ArrayList<Store> group;
 
-	public PayrollWriter(String we, String payoutFilePath, ArrayList<Store> group) {
-		payrollPath = payoutFilePath + "\\TipPayrollExport_" + we + ".csv";
-		this.group = group;
-	}
+  public PayrollWriter(String we, String payoutFilePath, ArrayList<Store> group)
+  {
+    payrollPath = payoutFilePath + "\\TipPayrollExport_" + we + ".csv";
+    this.group = group;
+  }
 
-	public void writePayrollExportCSV() {
-		try {
-			writer = new PrintWriter(new File(payrollPath));
-			for (Store s : group) {
-				for (Employee e : s.getEmployees()) {
-					if (s.getDollarTipPortion(e.getFullName()) > 0) {
-						String companyID = getCompanyIDForStore(s.getStoreNum());
-						String earDet = "E";
-						String detCode = "ER";
-						writer.println(String.format("%s,%d,%s,%s,,%.2f", companyID, e.getPayrollID(), earDet, detCode, s.getDollarTipPortion(e.getFullName())));
-					}
-				}
-			}
-			writer.flush();
-			writer.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+  public void writePayrollExportCSV()
+  {
+    try
+    {
+      writer = new PrintWriter(new File(payrollPath));
+      for (Store s : group)
+      {
+        for (Employee e : s.getEmployees())
+        {
+          if (s.getDollarTipPortion(e.getFullName()) > 0)
+          {
+            String companyID = getCompanyIDForStore(s.getStoreNum());
+            String earDet = "E";
+            String detCode = "TIPS";
+            writer.println(String.format("%d,%s,%s,0,%.2f,0,,,%s,02", e.getPayrollID(), earDet,
+                detCode, s.getDollarTipPortion(e.getFullName()), s.getStoreNum()));
+          }
+        }
+      }
+      writer.flush();
+      writer.close();
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
 
-	private String getCompanyIDForStore(int storeNum) {
-		switch(storeNum)
-		{
-		case 1131:
-			return "33776";
-		case 1300:
-			return "33777";
-		case 1528:
-			return "5554";
-		case 1740:
-			return "113027";
-		case 1741:
-			return "113028";
-		case 2048:
-			return "N5094";
-		case 2581:
-			return "11276";
-		case 3733:
-			return "124875";
-		}
-		return "";
-	}
+  private String getCompanyIDForStore(int storeNum)
+  {
+    switch (storeNum)
+    {
+      case 1131:
+        return "33776";
+      case 1300:
+        return "33777";
+      case 1528:
+        return "5554";
+      case 1740:
+        return "113027";
+      case 1741:
+        return "113028";
+      case 2048:
+        return "N5094";
+      case 2581:
+        return "11276";
+      case 3733:
+        return "124875";
+    }
+    return "";
+  }
 
 }
